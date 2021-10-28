e13e50051924f89481e463cd415c7d7435ff593f
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-28 20:28:06 +0530

    ADDED: code for simple linked list

--- com/bl/linkedlist/LinkedList.java
+++ com/bl/linkedlist/LinkedList.java
@@ -2,7 +2,22 @@ package com.bl.linkedlist;
 
 public class LinkedList {
 	public static void main(String[] args) {
-     System.out.println("linked list");
-	}
 
+		Node firstNode = new Node(56);
+		Node secondNode = new Node(70);
+		Node thirdNode = new Node(30);
+
+
+		Node head = firstNode;
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode); 
+		Node tail = thirdNode;
+
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.getData());
+			temp = temp.getNext();
+		}
+	}
 }
+
